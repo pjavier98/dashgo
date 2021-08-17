ddc86dc283aa332b4d423f13f4483f4da300c2f1
Pedro Javier <pjpc@ic.ufal.br>
2021-08-17 15:14:27 -0300

    feat: use mutation to create a new user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,15 +7,19 @@ import {
   HStack,
   SimpleGrid,
   VStack,
-} from "@chakra-ui/react";
-import Link from "next/link";
-import * as yup from "yup";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import * as yup from 'yup';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import { Sidebar } from "../../components/Sidebar";
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Form";
+import { Sidebar } from '../../components/Sidebar';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Form';
+import { useMutation } from 'react-query';
+import { api } from '../../services/axios';
+import { queryClient } from '../../services/queries/client';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -25,95 +29,115 @@ type CreateUserFormData = {
 };
 
 const createUserFormSchema = yup.object().shape({
-  name: yup.string().required("Nome é obrigatório"),
-  email: yup.string().required("Email é obrigatório").email("E-mail inválido"),
+  name: yup.string().required('Nome é obrigatório'),
+  email: yup.string().required('Email é obrigatório').email('E-mail inválido'),
   password: yup
     .string()
-    .required("Senha é obrigatória")
-    .min(6, "No mínimo 6 caracteres"),
+    .required('Senha é obrigatória')
+    .min(6, 'No mínimo 6 caracteres'),
   password_confirmation: yup
     .string()
-    .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
+    .oneOf([null, yup.ref('password')], 'As senhas precisam ser iguais'),
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post('users', {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      },
+    },
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
-    reValidateMode: "onBlur",
+    reValidateMode: 'onBlur',
   });
 
   const { errors } = formState;
 
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async data => {
+    await createUser.mutateAsync(data);
 
-    console.log("data => ", data);
+    router.push('/users');
   };
 
   return (
     <Box>
       <Header />
 
-      <Flex w={"100%"} my={"6"} maxWidth={"1480"} mx={"auto"} px={"6"}>
+      <Flex w={'100%'} my={'6'} maxWidth={'1480'} mx={'auto'} px={'6'}>
         <Sidebar />
 
         <Box
           as="form"
-          flex={"1"}
+          flex={'1'}
           borderRadius={8}
-          bg={"gray.800"}
-          p={["6", "8"]}
+          bg={'gray.800'}
+          p={['6', '8']}
           onSubmit={handleSubmit(handleCreateUser)}
         >
-          <Heading size={"lg"} fontWeight={"normal"}>
+          <Heading size={'lg'} fontWeight={'normal'}>
             Criar usuário
           </Heading>
 
-          <Divider my={"6"} borderColor={"gray.700"} />
+          <Divider my={'6'} borderColor={'gray.700'} />
 
-          <VStack spacing={"8"}>
-            <SimpleGrid minChildWidth={"240px"} spacing={["6", "8"]} w={"100%"}>
+          <VStack spacing={'8'}>
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w={'100%'}>
               <Input
-                name={"name"}
-                label={"Nome completo"}
-                {...register("name")}
+                name={'name'}
+                label={'Nome completo'}
+                {...register('name')}
                 error={errors.name}
               />
               <Input
-                name={"email"}
-                label={"E-mail"}
-                {...register("email")}
+                name={'email'}
+                label={'E-mail'}
+                {...register('email')}
                 error={errors.email}
               />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={"240px"} spacing={["6", "8"]} w={"100%"}>
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']} w={'100%'}>
               <Input
-                name={"password"}
+                name={'password'}
                 type="password"
-                label={"Senha"}
-                {...register("password")}
+                label={'Senha'}
+                {...register('password')}
                 error={errors.password}
               />
               <Input
-                name={"password_confirmation"}
+                name={'password_confirmation'}
                 type="password"
-                label={"Confirmação da senha"}
-                {...register("password_confirmation")}
+                label={'Confirmação da senha'}
+                {...register('password_confirmation')}
                 error={errors.password_confirmation}
               />
             </SimpleGrid>
           </VStack>
-          <Flex mt={"8"} justify={"flex-end"}>
-            <HStack spacing={"4"}>
-              <Link href={"/users"} passHref>
-                <Button as={"a"} colorScheme={"whiteAlpha"}>
+          <Flex mt={'8'} justify={'flex-end'}>
+            <HStack spacing={'4'}>
+              <Link href={'/users'} passHref>
+                <Button as={'a'} colorScheme={'whiteAlpha'}>
                   Cancelar
                 </Button>
               </Link>
               <Button
                 type="submit"
-                colorScheme={"pink"}
+                colorScheme={'pink'}
                 isLoading={formState.isSubmitting}
               >
                 Salvar
