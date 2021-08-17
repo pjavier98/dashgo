edfbc21283d47886044ed1146b0d5dfe7137625c
Pedro Javier <pjpc@ic.ufal.br>
2021-08-17 14:36:39 -0300

    feat: add page logic to pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,24 +1,88 @@
-import { Box, HStack, Stack } from "@chakra-ui/react";
-import { PaginationItem } from "./PaginationItem";
+import { Box, HStack, Stack, Text } from '@chakra-ui/react';
+import { PaginationItem } from './PaginationItem';
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registerPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => from + index + 1)
+    .filter(page => page > 0);
+}
+
+export function Pagination({
+  currentPage = 1,
+  registerPerPage = 10,
+  totalCountOfRegisters,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registerPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage),
+        )
+      : [];
+
   return (
     <Stack
-      direction={["column", "row"]}
-      spacing={"6"}
-      mt={"8"}
-      justify={"space-between"}
-      align={"center"}
+      direction={['column', 'row']}
+      spacing={'6'}
+      mt={'8'}
+      justify={'space-between'}
+      align={'center'}
     >
       <Box>
-        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+        <strong>{currentPage}</strong> - <strong>{registerPerPage}</strong> de{' '}
+        <strong>{totalCountOfRegisters}</strong>
       </Box>
-      <HStack spacing={"2"}>
-        <PaginationItem number={1} isCurrent />
+      <HStack spacing={'2'}>
+        {currentPage > 1 + siblingsCount && (
+          <>
+            <PaginationItem number={1} />
+            {currentPage > 2 + siblingsCount && (
+              <Text color={'gray.300'} width={'8'} textAlign={'center'}>
+                ...
+              </Text>
+            )}
+          </>
+        )}
 
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
+        {previousPages.length > 0 &&
+          previousPages.map(page => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        <PaginationItem number={currentPage} isCurrent />
+
+        {nextPages.length > 0 &&
+          nextPages.map(page => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            {currentPage + siblingsCount + 1 < lastPage && (
+              <Text color={'gray.300'} width={'8'} textAlign={'center'}>
+                ...
+              </Text>
+            )}
+            <PaginationItem number={lastPage} />
+          </>
+        )}
       </HStack>
     </Stack>
   );
