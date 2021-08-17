435268af93dd40ee5222e2c33f8fd03513c7f886
Pedro Javier <pjpc@ic.ufal.br>
2021-08-17 11:58:55 -0300

    feat: use react-query to get users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -29,7 +29,20 @@ export default function UserList() {
     const response = await fetch('/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      };
+    });
+
+    return users;
   });
 
   const isWideVersion = useBreakpointValue({
@@ -85,33 +98,39 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme={'pink'} />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight={'bold'}>Pedro Javier</Text>
-                        <Text fontSize={'small'} color={'gray.300'}>
-                          pjpc@outlook.com.br
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    {isWideVersion && (
-                      <Td>
-                        <Button
-                          as={'a'}
-                          size={'sm'}
-                          fontSize={'sm'}
-                          colorScheme={'purple'}
-                          leftIcon={<Icon as={RiPencilLine} fontSize={'16'} />}
-                        >
-                          Editar
-                        </Button>
-                      </Td>
-                    )}
-                  </Tr>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme={'pink'} />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight={'bold'}>{user.name}</Text>
+                            <Text fontSize={'small'} color={'gray.300'}>
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        {isWideVersion && (
+                          <Td>
+                            <Button
+                              as={'a'}
+                              size={'sm'}
+                              fontSize={'sm'}
+                              colorScheme={'purple'}
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize={'16'} />
+                              }
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        )}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
